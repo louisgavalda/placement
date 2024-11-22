e7b6a1a57b511477c944d5959564cac8519ba293
Louis <louis@gavalda.fr>
2024-11-22 17:27:48 +0100

    test function import

--- scripts/utils.jsx
+++ scripts/utils.jsx
@@ -1,19 +1,3 @@
-function closePath(item) {
-    if (item.closed) return;
-
-    var points = item.pathPoints;
-    var point = {
-        start: points[0],
-        end: points[points.length - 1]
-    };
-
-    if (!hasLeftHandle(point.start) && hasRightHandle(point.start)) {
-        setLeftHandlePosition(point.start);
-    }
-
-    if (hasLeftHandle(point.end) && !hasRightHandle(point.end)) {
-        setRightHandlePosition(point.end);
-    }
-
-    item.closed = true;
+function alertMessage() {
+    alert("Hello World!");
 }
