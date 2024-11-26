da257c0c6c6a4e74c1965be3bb21b79e089d2081
Louis <louis@gavalda.fr>
2024-11-26 12:23:54 +0100

    ✨ message de confirmation

--- disableExternalScriptWarning.js
+++ disableExternalScriptWarning.js
@@ -1,2 +1,4 @@
 // Disable the warning message when opening external JS/JSX files in Adobe Illustrator.
 app.preferences.setBooleanPreference("ShowExternalJSXWarning", false);
+
+alert("Confirmation dialog successfully disabled!");
